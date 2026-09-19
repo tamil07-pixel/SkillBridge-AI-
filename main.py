078c692123a200c0b002106c4b018ee6b431ecde
Tamilarasan <tamil200607@gmail.com>
2026-09-19 14:12:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel
+("gemini-2.5-flash")
 # App Header
 st.title("🚀 SkillBridge AI")
 st.caption("AI-powered Career Roadmap Generator & Job Scam Detector")
